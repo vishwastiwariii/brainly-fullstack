afffe75eb008d0bc942764d68a9c99f0e7b49623
vishwastiwariii <vishwastiwariii09@gmail.com>
2026-01-12 23:24:04 +0530

    Added delete Category route

---
+++ backend/src/controllers/categoryController.js
@@ -0,0 +1,18 @@
+import Category from '../models/Category.js'
+
+
+export async function getAllCategory(req, res){
+    
+}
+
+export async function updateCategory(req, res){
+
+}
+
+export async function createCategory(req, res){
+
+}
+
+export async function deleteCategory(req, res){
+
+}
--- backend/src/routes/categoryRoutes.js
+++ backend/src/routes/categoryRoutes.js
@@ -17,9 +17,8 @@ const router = express.Router()
 
 router.get('/', limiter, authenticate)    // show all categories
 router.patch('/update/:id', limiter, authenticate)  // updates a category
-router.post('/create', limiter, authenticate)    // create a new card
-router.get('/:userId', limiter, authenticate)   // fetch all categories of a user
-
+router.post('/create', limiter, authenticate)    // create a new category
+router.delete('/delete/:id', limiter, authenticate) // delete a category
 
 
 export default router
